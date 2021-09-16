05a855e26d8f05e557d170234890487f9530dd75
Whitef1eld <nwhitefiel1@etf.unsa.ba>
2021-09-16 14:41:40 +0200

    Adding the strings

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    // only change code below this line
+    var myFirstName = "Nandin";
+    var myLastName = "Whitefield";
+    var myHomeTown = "Sarajevo";
+    // only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
